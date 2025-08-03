13033d183d3283891b4030c80b8e1690ce1915fa
okabe <koo710128@gmail.com>
2025-08-03 23:34:06 +0900

    #1 meta追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import Header from "./components/layout/Header";
 import "./styles/globals.css";
 
+export const metadata = {
+  title: "Local Taskline | ローカルでタスク管理",
+  description: "Local Taskline | ローカルでタスク管理",
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
