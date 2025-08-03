b39b4330090fc2b6e445bc5ad29ce34899ea830b
okabe <koo710128@gmail.com>
2025-08-03 23:09:53 +0900

    #1 フィルターandソート機能追加

--- src/app/components/FilterBar.tsx
+++ src/app/components/FilterBar.tsx
@@ -1,8 +1,17 @@
-export default function FilterBar() {
+export default function FilterBar({ 
+  filterText, 
+  setFilterText, 
+  sortOrder, 
+  setSortOrder 
+}:{ 
+  filterText: string, 
+  setFilterText: (text: string) => void, 
+  sortOrder: string, setSortOrder: (order: string) => void 
+}){
   return (
     <div className="flex gap-4 items-center">
-      <span>Filter:　<input className="border-1 border-gray-300 rounded-md p-1" type="text" /></span>
-      <span>Sort:　<select name="sort" id="sort" className="border-1 border-gray-300 rounded-md p-1">
+      <span>Filter:　<input className="border-1 border-gray-300 rounded-md p-1" type="text" value={filterText} onChange={(e) => setFilterText(e.target.value)}/></span>
+      <span>Sort:　<select name="sort" id="sort" className="border-1 border-gray-300 rounded-md p-1 md:cursor-pointer" value={sortOrder} onChange={(e) => setSortOrder(e.target.value)}>
         <option value="newest">新着順</option>
         <option value="oldest">古い順</option>
         <option value="a-z">期限昇順</option>
--- src/app/components/TaskCardArea.tsx
+++ src/app/components/TaskCardArea.tsx
@@ -1,14 +1,12 @@
 'use client';
 import TaskCard from "./TaskCard";
-// import { MOCK_TASKS } from "../lib/mockData";
-import { useTaskStore } from "../lib/store";
+import { Task } from "../lib/types";
 
 
-export default function TaskCardArea() {
-  // const TaskList= MOCK_TASKS;
-
-  const TaskList = useTaskStore((state) => state.tasks);
-
+export default function TaskCardArea(
+  { filteredAndSortedTasks }: { filteredAndSortedTasks: Task[] }
+) {
+  const TaskList = filteredAndSortedTasks;
   
   return (
     <div className="grid md:grid-cols-3 grid-cols-1 gap-6 mt-4">
